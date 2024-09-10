24f3ede1910cbe35c318bbd410667fa445355ab5
mutebinuhu <mutebinuhu1@gmail.com>
2024-09-10 18:49:16 +0400

    chore: few changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://testgit.laverde.ae',
+  base:'https://testgit.laverde.ae/dist',
   base:'https://admin.laverde.ae',
   plugins: [react()],
 })
